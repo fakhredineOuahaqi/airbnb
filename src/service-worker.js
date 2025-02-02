fe455942b148fde89bb3b9211362c282202f1297
fakhredineOuahaqi <aitouahaqi.fakhereddine@gmail.com>
2025-02-02 03:21:12 +0100

    Add dist/ to .gitignore and remove from tracking

---
+++ src/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'earnings-tracker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  // Add all other assets that need to be cached
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache:', CACHE_NAME);
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  clients.claim();
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
