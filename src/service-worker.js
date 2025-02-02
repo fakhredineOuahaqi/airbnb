ffd48d0785aeea61b14725ae8626f63c1226821f
fakhredineOuahaqi <aitouahaqi.fakhereddine@gmail.com>
2025-02-02 03:28:14 +0100

    update service

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,19 +3,27 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  // Add all other assets that need to be cached
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/dist/output.css',
 ];
 
+// Handle install event
 self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache:', CACHE_NAME);
-      return cache.addAll(urlsToCache);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.error('Error caching files during install:', err);
+      })
   );
 });
 
+// Handle activate event
 self.addEventListener('activate', (event) => {
   clients.claim();
   event.waitUntil(
@@ -29,6 +37,7 @@ self.addEventListener('activate', (event) => {
   );
 });
 
+// Handle fetch events
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
